2061af9efe1ba4c8c5b9a5693a3b9c251ff33836
Ramesh Shrestha <shresthashare@gmail.com>
2021-02-02 14:08:59 -0600

    updated app with a shop page, collectioin preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
-import {Route, Link} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 
-
-const TopicsList=()=>{
-  return(
-    <h1>Topic List page</h1>
-  )
-}
-const TopicsDetails=(props)=>{
-  console.log(props)
-  return(
-    <h1>Topic Details page</h1>
-  )
-}
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/hats' component={TopicsList}/>
-      <Route path='/topics/:topicId' component={TopicsDetails}/>
+      <Route exact path='/shop' component={ShopPage}/>
     </div>
   );
 }
